4388325daa422ba76b32848ee08d8ce3663c8e54
Filipemtb <filipera3@gmail.com>
2024-05-27 14:30:19 -0300

    expressoes lambda: comparator v6

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import model.entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -15,9 +14,7 @@ public class Main {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = (p1, p2) ->  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        list.sort(comp);
+        list.sort((p1, p2) ->  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
         
         for (Product p : list) {
             System.out.println(p);
